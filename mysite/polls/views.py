318912abb41f7caf2df2979b4035977ff023c18f
Anders Westrheim <anders.westrheim@gmail.com>
2021-03-16 23:42:27 +0100

    Updated tests

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,9 @@ from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
+
 from .models import Question, Choice
 
 
@@ -11,11 +13,15 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """Avoid stuff in the future"""
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
